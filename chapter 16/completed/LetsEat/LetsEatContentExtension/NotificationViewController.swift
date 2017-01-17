980c88bdce0b3057e230c3524d66ba5d635bc50c
Craig Clayton <craig@cocoa.academy>
2017-01-17 08:53:36 -0500

    update files

---
+++ chapter 16/completed/LetsEat/LetsEatContentExtension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  LetsEatContentExtension
+//
+//  Created by Craig Clayton on 11/26/16.
+//  Copyright © 2016 Craig Clayton. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
